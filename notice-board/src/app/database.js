c5669ac0aa240767c08a30a4f63cbc14d35abdb2
i_junghoon <ljh37694@naver.com>
2024-08-14 13:47:07 +0900

    mongodb 연결

--- notice-board/src/app/database.js
+++ notice-board/src/app/database.js
@@ -1,15 +1,15 @@
 import { MongoClient } from "mongodb";
-const url = `mongodb+srv://ljh37694:${process.env.DB_PASSWORD}@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority`;
+const url = `mongodb+srv://ljh37694:${process.env.DB_PASSWORD}@forum.6p5dx3j.mongodb.net/?retryWrites=true&w=majority&appName=Forum`;
 
 const options = { useNewUrlParser: true };
 let connectDB;
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongo) {
-    global._mongo = new MongoClient(url, options).connect()
+    global._mongo = new MongoClient(url, options).connect();
   }
   connectDB = global._mongo
 } else {
-  connectDB = new MongoClient(url, options).connect()
+  connectDB = new MongoClient(url, options).connect();
 }
 export { connectDB }
--- notice-board/src/app/page.js
+++ notice-board/src/app/page.js
@@ -3,7 +3,7 @@ import { connectDB } from "./database";
 export default async function Home() {
   let client = await connectDB;
   const db = client.db('NextForum');
-  let result = await db.collection('post').find().toArray();
+  let result = await db.collection('posts').find().toArray();
 
   return (
     <div>
